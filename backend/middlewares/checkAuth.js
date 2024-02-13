30fb665880c03dc7002229270c712fecb0fae294
Davide Ceresa <ceredavide@live.it>
2024-02-13 17:20:24 +0100

    feat: function which validates the token

---
+++ backend/middlewares/checkAuth.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken")
+
+const HttpError = require("../classes/HttpError")
+const User = require("../models/User")
+
+module.exports = async (req, res, next) => {
+
+    let decodedToken;
+
+    let user;
+
+    try {
+        const token = req.headers.authorization.split(" ")[1]
+
+        if (!token) {
+            throw new Error("Token not valid.")
+        }
+
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+
+    } catch (err) {
+        return next(new HttpError("Unauthorised", 401))
+    }
+
+    try {
+        user = await User.findById(decodedToken.id)
+    } catch (err) {
+        return next(new HttpError("Token not valid."))
+    }
+
+    if (!user) {
+        return next(new HttpError("Token not valid."))
+    }
+
+    console.log("Utente connesso: " + user.id)
+
+    req.userData = user;
+
+    return next()
+}
