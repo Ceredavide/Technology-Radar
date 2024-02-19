370200f037d1476ebf4b05d9bf47401e303c44e2
Davide Ceresa <ceredavide@live.it>
2024-02-19 13:25:57 +0100

    feat: added home component & fixed auth components path

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes, mapToCanActivate } from '@angular/router';
 
-import { LoginComponent } from './modules/auth/components/login/login.component';
-import { SignupComponent } from './modules/auth/components/signup/signup.component';
+import { LoginComponent } from './core/auth/components/login/login.component';
+import { SignupComponent } from './core/auth/components/signup/signup.component';
 
 import { TechnologyFormComponent } from "./modules/admin/components/technology-form/technology-form.component"
 import { AuthGuard } from './core/guards/auth/auth.guard';
+import { TechnologyRadarComponent } from './modules/home/components/technology-radar/technology-radar.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'home', component: TechnologyRadarComponent, canActivate: mapToCanActivate([AuthGuard]) },
     { path: 'form', component: TechnologyFormComponent, canActivate: mapToCanActivate([AuthGuard]) },
-    { path: '', redirectTo: '/form', pathMatch: 'full' }
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
