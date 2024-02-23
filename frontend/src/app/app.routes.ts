5957c5aa7df163e22c1b78948849a951ae80efb7
Davide Ceresa <ceredavide@live.it>
2024-02-23 18:25:16 +0100

    feat: splitted route into modules and added publish page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes, mapToCanActivate } from '@angular/router';
 
+import { AuthGuard } from './core/guards/auth/auth.guard';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { SignupComponent } from './core/auth/components/signup/signup.component';
 
 import { TechnologyFormComponent } from "./modules/admin/components/technology-form/technology-form.component"
-import { AuthGuard } from './core/guards/auth/auth.guard';
 import { TechnologyRadarComponent } from './modules/home/components/technology-radar/technology-radar.component';
+
+import { AdminScreenComponent } from './modules/admin/components/admin-screen/admin-screen.component';
+import { TechnologyDashboardComponent } from './modules/admin/components/technology-dashboard/technology-dashboard.component';
+import { RingHandlerComponent } from './modules/admin/components/ring-handler/ring-handler.component';
 
 export const routes: Routes = [
     {
@@ -13,7 +17,7 @@ export const routes: Routes = [
         children: [
             { path: 'login', component: LoginComponent },
             { path: 'signup', component: SignupComponent },
-        ]
+        ],
     },
     {
         path: "home",
@@ -23,8 +27,12 @@ export const routes: Routes = [
     {
         path: 'admin',
         canActivate: mapToCanActivate([AuthGuard]),
+        component: AdminScreenComponent,
         children: [
-            { path: 'form', component: TechnologyFormComponent }
+            { path: "", redirectTo: '/admin/dashboard', pathMatch: 'full' },
+            { path: "dashboard", component: TechnologyDashboardComponent },
+            { path: 'create-technology', component: TechnologyFormComponent },
+            { path: 'technology/:id/publish', component: RingHandlerComponent }
         ],
     },
     { path: '**', redirectTo: '/home', pathMatch: 'full' }
