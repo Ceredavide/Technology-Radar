1d2e777bcb5c0d48556893e189e6e592e427a3d5
Davide Ceresa <ceredavide@live.it>
2024-02-23 18:33:38 +0100

    feat: added fields used from admin

--- frontend/src/app/shared/interfaces/Technology.ts
+++ frontend/src/app/shared/interfaces/Technology.ts
@@ -1,7 +1,17 @@
+import User from "../../core/auth/interfaces/User";
+
 export default interface Technology {
     name: string,
     description: string,
     ring: string,
     category: string,
-    descriptionCategorization: string
+    descriptionCategorization: string,
+    _id?: string,
+    creator?: User,
+    publisher?: User,
+    published?: boolean,
+    createdAt?: string,
+    publishedAt?: string,
+    updatedAt?: string,
+    __v?: number
 }
