80fe30cb1bf6e8aac5c526c14e59b61ee5300f77
Davide Ceresa <ceredavide@live.it>
2024-02-23 18:28:30 +0100

    feat: added fetchAllTechnology and publishTechnology

--- backend/controllers/technology.js
+++ backend/controllers/technology.js
@@ -1,17 +1,34 @@
 const HttpError = require("../classes/HttpError")
 const Technology = require("../models/Technology")
+
+const addIncompleteTechnologies = require("../utils/addIncompleteTechnologies")
 const formatErrors = require("../utils/formatErrors")
 const formatTechnologies = require("../utils/formatTechnologies")
 
 //
 //GET
 //
-exports.getPublished = async (req, res, next) => {
+exports.getTechnologyById = async (req, res, next) => {
+
+    const technologyId = req.params.id
+
+    let technology
+
+    try {
+        technology = await Technology.findById(technologyId, ' -published -creator -createdAt -updatedAt -__v')
+    } catch (err) {
+        return next(new HttpError("Something went wrong, try again later.", 500))
+    }
+
+    res.status(200).json(technology)
+}
+
+exports.getPublishedTechnologies = async (req, res, next) => {
 
     let technologies
 
     try {
-        technologies = await Technology.find({ published: true }, '-published -creator -createdAt -updatedAt -__v')
+        technologies = await Technology.find({ published: true }, '-_id -published -creator -createdAt -updatedAt -__v')
     } catch (err) {
         return next(new HttpError("Something went wrong, try again later.", 500))
     }
@@ -21,10 +38,28 @@ exports.getPublished = async (req, res, next) => {
     res.status(200).json(formattedTechnologies)
 }
 
+exports.getAllTechnologies = async (req, res, next) => {
+
+    let technologies
+
+    try {
+        technologies = await Technology.find({}).populate("creator publisher","-_id -password -createdAt -updatedAt -role -__v")
+    } catch (err) {
+        return next(new HttpError("Something went wrong, try again later.", 500))
+    }
+
+    const formattedTechnologies = formatTechnologies(technologies)
+
+    const formattedTechnologiesWithIncompleteFields = addIncompleteTechnologies(technologies, formattedTechnologies)
+
+    res.status(200).json(formattedTechnologiesWithIncompleteFields)
+}
+
+
 //
 //POST
 //
-exports.create = async (req, res, next) => {
+exports.createTechnology = async (req, res, next) => {
 
     const { _id: userId } = req.userData
 
@@ -35,6 +70,8 @@ exports.create = async (req, res, next) => {
         ring,
         descriptionCategorization
     } = req.body
+
+    console.log(req.body)
 
     const technology = new Technology({
         name,
@@ -59,4 +96,53 @@ exports.create = async (req, res, next) => {
     }
 
     res.status(201).json(technology.toObject())
+}
+
+//
+//  PUT
+//
+exports.publishTechnology = async (req, res, next) => {
+
+    const { _id: userId } = req.userData
+
+    const technologyId = req.params.id
+
+    const {
+        ring,
+        descriptionCategorization
+    } = req.body
+
+    let technology
+
+    try {
+        technology = await Technology.findById(technologyId, ' -published -creator -createdAt -updatedAt -__v')
+    } catch (err) {
+        return next(new HttpError("Something went wrong, try again later.", 500))
+    }
+
+    if (!technology) {
+        return next(new HttpError("Technology not found.", 404))
+    }
+
+    technology.ring = ring;
+    technology.descriptionCategorization = descriptionCategorization;
+    technology.publisher = userId;
+    technology.publishedAt = new Date()
+    technology.published = true
+    
+    const errors = technology.validateSync()
+
+    if (errors) {
+        errorsString = formatErrors(Object.values(errors.errors))
+        console.log(errorsString)
+        return next(new HttpError("Bad Request:" + errorsString, 400))
+    }else{
+        try {
+            await technology.save();
+        } catch (err) {
+            return next(new HttpError("Internal Server Error, please try again later.", 500))
+        }
+    }
+
+    res.status(200).json(technology.toObject())
 }
