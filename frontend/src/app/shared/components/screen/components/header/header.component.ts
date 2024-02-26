78a7a87574bbffe2d73e179495d76b6d34c68b44
Davide Ceresa <ceredavide@live.it>
2024-02-27 00:43:32 +0100

    feat: implemented view user info and buttons go to admin page / logout

--- frontend/src/app/shared/components/screen/components/header/header.component.ts
+++ frontend/src/app/shared/components/screen/components/header/header.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+
+import User from '../../../../../core/auth/interfaces/User';
+
+import { UserService } from '../../../../../core/auth/services/user/user.service';
+import { AuthService } from '../../../../../core/auth/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
+  userData: User | null
+
+  isAdmin : boolean = false
+
+  isMobileMenuOpen: boolean = false
+
+  constructor(private router: Router, private userService: UserService, private authService: AuthService) {
+    this.userData = this.userService.getUser()
+    this.isAdmin = this.userService.hasRole(["Chief Technology Officer"])
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen
+  }
+
+  logOut(){
+    this.authService.logout()
+    this.router.navigateByUrl("auth/login")
+  }
 }
