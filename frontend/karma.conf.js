7d9a2c31f23886a72b41b9fbe0f445d5d8050165
Davide Ceresa <ceredavide@live.it>
2024-04-21 03:20:20 +0200

    ci: changed node-version and corrected angular CI

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,18 +1,27 @@
 module.exports = function (config) {
-    config.set({
+  config.set({
+    // ...
+    plugins: [
       // ...
-      plugins: [
-        // ...
-        require('karma-coverage'),
-        require('karma-coverage-istanbul-reporter')
-      ],
-      // ...
-      coverageIstanbulReporter: {
-        dir: require('path').join(__dirname, './coverage'),
-        reports: ['html', 'lcovonly', 'text-summary'],
-        fixWebpackSourcePaths: true
+      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
+      require('karma-chrome-launcher')
+    ],
+    // ...
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
+    // ...
+    browsers: ['ChromeHeadlessCustom'],
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        // flags, etc.
       },
-      reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
-      // ...
-    });
-  };
+    },
+    // ...
+  });
+};
