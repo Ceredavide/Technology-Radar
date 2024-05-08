f35727e099c36ae2b340f409a118b2cbc867dd00
Davide Ceresa <ceredavide@live.it>
2024-05-08 11:53:42 +0200

    chore: Update karma.conf.js to use ChromeHeadlessCustom launcher with specific flags

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,27 +1,17 @@
-module.exports = function (config) {
+// karma.conf.js
+module.exports = function(config) {
   config.set({
-    // ...
-    plugins: [
-      // ...
-      require('karma-coverage'),
-      require('karma-coverage-istanbul-reporter'),
-      require('karma-chrome-launcher')
-    ],
-    // ...
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
-    // ...
+    // other configuration settings...
+
     browsers: ['ChromeHeadlessCustom'],
+
     customLaunchers: {
       ChromeHeadlessCustom: {
         base: 'ChromeHeadless',
-        // flags, etc.
-      },
+        flags: ['--no-sandbox', '--disable-gpu', '--remote-debugging-port=9222']
+      }
     },
-    // ...
+
+    // continue with other configurations...
   });
 };
