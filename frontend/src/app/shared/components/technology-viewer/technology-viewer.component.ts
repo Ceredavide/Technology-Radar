d996ea9f131a97c35a4a51c1090c848fc4a211f9
Davide Ceresa <ceredavide@live.it>
2024-05-08 10:51:49 +0200

    refactor: Update technology-viewer component to emit technologiesChanged event instead of setSelectedTechnologies

--- frontend/src/app/shared/components/technology-viewer/technology-viewer.component.ts
+++ frontend/src/app/shared/components/technology-viewer/technology-viewer.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-
 import TechnologyViewer from '../../interfaces/TechnologyViewer';
 import Technology from '../../interfaces/Technology';
 import Ring from '../../interfaces/Ring';
@@ -7,37 +6,35 @@ import Ring from '../../interfaces/Ring';
 import { RingTechnologies } from './components/ring-technologies/ring-technologies.component';
 import { CategoryTabs } from './components/category-tabs/category-tabs.component';
 
-
 @Component({
   selector: 'technology-viewer',
   standalone: true,
-  imports: [
-    RingTechnologies,
-    CategoryTabs
-  ],
+  imports: [RingTechnologies, CategoryTabs],
   templateUrl: './technology-viewer.component.html'
 })
 export class TechnologyViewerComponent implements OnInit, OnChanges {
-
-  @Input() technologyRadar!: TechnologyViewer[];
-  @Output() setSelectedTechnologies = new EventEmitter<Technology[]>();
+  @Input() technologyRadar: TechnologyViewer[] = [];
+  @Output() technologiesChanged = new EventEmitter<Technology[]>();
 
   categories: string[] = [];
-  selectedRings : Ring[] = []
-  selectedCategoryIndex: number = 0;
-  selectedRingIndex: number = 0;
+  selectedRings: Ring[] = [];
+  selectedCategoryIndex = 0;
+  selectedRingIndex = 0;
   selectedTechnologies: Technology[] = [];
 
   ngOnInit(): void {
-    this.updateCategories();
-    this.updateSelections();
+    this.refreshData();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes["technologyRadar"]) {
-      this.updateCategories();
-      this.updateSelections();
+    if (changes['technologyRadar']) {
+      this.refreshData();
     }
+  }
+
+  refreshData(): void {
+    this.updateCategories();
+    this.updateSelections();
   }
 
   updateCategories(): void {
@@ -47,7 +44,7 @@ export class TechnologyViewerComponent implements OnInit, OnChanges {
   updateSelections(): void {
     this.selectedRings = this.technologyRadar[this.selectedCategoryIndex]?.rings || [];
     this.selectedTechnologies = this.selectedRings[this.selectedRingIndex]?.technologies || [];
-    this.setSelectedTechnologies.emit(this.selectedTechnologies);
+    this.technologiesChanged.emit(this.selectedTechnologies);
   }
 
   categoryChange(index: number): void {
@@ -60,4 +57,3 @@ export class TechnologyViewerComponent implements OnInit, OnChanges {
     this.updateSelections();
   }
 }
-
